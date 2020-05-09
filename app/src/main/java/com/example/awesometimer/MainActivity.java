7633d8bd2bcdf41b386695c6db75b89a0284a575
PeterOlache <64915234+PeterOlache@users.noreply.github.com>
2020-05-09 11:57:11 -0500

    added comments and partial of page 2 working

--- app/src/main/java/com/example/awesometimer/MainActivity.java
+++ app/src/main/java/com/example/awesometimer/MainActivity.java
@@ -3,20 +3,13 @@ package com.example.awesometimer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.CountDownTimer;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //global variables for input
     private TextView sets;
     private TextView reps;
     private CheckBox uE;
@@ -26,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // gets the user input
         sets = findViewById(R.id.sets);
         reps = findViewById(R.id.reps);
         uE = findViewById(R.id.uniqueExercise);
@@ -33,12 +27,17 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addTime(View v) {
+        // converts user the user input into usable data ints
         int numSets = Integer.parseInt(sets.getText().toString());
         int numReps = Integer.parseInt(reps.getText().toString());
+        // if statements that convert checked for 1 and unchecked for 2
         int uniqueEx = (uE.isChecked()) ? 1 : 0 ;
         int uniqueRe = (uR.isChecked()) ? 1 : 0 ;
+
+        //Data that will be passed to next page
         int[] info = {numSets, numReps, uniqueEx, uniqueRe};
         Intent intent;
+        //determins which page they will be redirected to
         if (uniqueEx == 1 && uniqueRe == 0) {
             intent = new Intent(this, exerciseOnly.class);
         } else if (uniqueEx == 0 && uniqueRe == 1){
@@ -48,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
         } else{
             intent = new Intent(this, noneUnique.class);
         }
+        //adds info to the intent so it can be accessed from the next page
         intent.putExtra("info", info);
 
         startActivity(intent);
--- app/src/main/java/com/example/awesometimer/exerciseOnly.java
+++ app/src/main/java/com/example/awesometimer/exerciseOnly.java
@@ -4,21 +4,72 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class exerciseOnly extends AppCompatActivity {
     private TextView repsTime;
     private TextView repsRest;
     private TextView setsRest;
+    private TextView repsTimeText;
+    private TextView restTimeText;
+    private TextView setsRestText;
+    private int[] info;
+    private ArrayList<Integer> timer = new ArrayList<Integer>();;
+    private int count;
+    private  int repRest;
+    private  int set;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_only);
 
+        count = 1;
         Intent intent = getIntent();
-        int[] info = intent.getIntArrayExtra("info");
+        info = intent.getIntArrayExtra("info");
         for (int element: info) {
             System.out.println(element);
         }
+        repsTime = findViewById(R.id.exerciseLength);
+        repsRest = findViewById(R.id.restExersice);
+        setsRest = findViewById(R.id.restSets);
+        repsTimeText = findViewById(R.id.exerciseRest);
+        restTimeText = findViewById(R.id.restTime);
+        setsRestText = findViewById(R.id.setsRest);
+    }
+
+    public void next(View v){
+        System.out.println("next start");
+        int rep = Integer.parseInt(repsTime.getText().toString());
+        if (count == 1){
+            repRest = Integer.parseInt(repsRest.getText().toString());
+            set= Integer.parseInt(setsRest.getText().toString());
+            restTimeText.setVisibility(View.INVISIBLE);
+            setsRestText.setVisibility(View.INVISIBLE);
+            repsRest.setVisibility(View.INVISIBLE);
+            setsRest.setVisibility(View.INVISIBLE);
+        }
+        timer.add(rep);
+        timer.add(repRest);
+        count++;
+        repsTime.setText("0");
+        repsTimeText.setText("Exercise " + count + " length");
+        for (int element: timer) {
+            System.out.println(element);
+        }
+        if (count >= info[0]+1){
+            pageDone();
+        }
+    }
+
+    public void pageDone(){
+        for(int i=0; i < info[1]; i++){
+
+        }
+        System.out.println("pageDone");
     }
 }
