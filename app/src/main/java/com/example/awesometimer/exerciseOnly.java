7e56f74bbba96170f29123dd9ee52c277b50f7f4
N4V3H3114 <ecallen@noctrl.edu>
2020-09-27 15:56:09 -0500

    Changed labels for text in the exerciseOnly file, and added them to the strings.xml for better maintenance (we can call on these later for other layouts now). Added functionality to add exercise button that will now update the text on which exercise is being added.

--- app/src/main/java/com/example/awesometimer/exerciseOnly.java
+++ app/src/main/java/com/example/awesometimer/exerciseOnly.java
@@ -4,12 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class exerciseOnly extends AppCompatActivity {
-    private TextView repsTime;
+    private TextView eLE;
     private TextView repsRest;
     private TextView setsRest;
+    private TextView eLL;
+    private int exercises;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //If user has unique exercise but the same rest time
@@ -21,7 +25,15 @@ public class exerciseOnly extends AppCompatActivity {
         for (int element: info) { //Debug code to check if data is getting passed correctly
             System.out.println(element);
         }
+        exercises = 1;
+        eLL = findViewById(R.id.exerciseOnlyLengthLabel); //exercise length label
+        eLE = findViewById(R.id.exerciseOnlyLengthEdit); //exercise length edit
 
 
+    }
+
+    public void addExercise(View v){
+        exercises++;
+        eLL.setText("Exercise " + exercises + " length");
     }
 }
