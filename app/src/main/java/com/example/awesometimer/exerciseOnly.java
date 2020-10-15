d1aba7a2cb028fadd2f6f2bdcd8bab5c4d6cf2c6
N4V3H3114 <ecallen@noctrl.edu>
2020-10-14 21:46:59 -0500

    Minor changes. Trying to add a new button to the layout to act as the start button. Can't place it very well.

--- app/src/main/java/com/example/awesometimer/exerciseOnly.java
+++ app/src/main/java/com/example/awesometimer/exerciseOnly.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class exerciseOnly extends AppCompatActivity {
-    private TextView eLE;
     private TextView repsRest;
     private TextView setsRest;
     private TextView eLL;
+    private EditText eLE;
+    private EditText eRE;
+    private EditText eNE;
+    private EditText eRSE;
     private int exercises;
 
     @Override
@@ -28,12 +32,18 @@ public class exerciseOnly extends AppCompatActivity {
         exercises = 1;
         eLL = findViewById(R.id.exerciseOnlyLengthLabel); //exercise length label
         eLE = findViewById(R.id.exerciseOnlyLengthEdit); //exercise length edit
-
+        eRE = findViewById(R.id.exerciseOnlyRestEdit); // exercise rest edit
+        eNE = findViewById(R.id.exerciseOnlyNameEdit); // exercise name edit
+        eRSE = findViewById(R.id.exerciseOnlyRestSetEdit); // exercise rest for the set
 
     }
 
     public void addExercise(View v){
         exercises++;
         eLL.setText("Exercise " + exercises + " length");
+    }
+
+    public void beginWorkout(View v){
+
     }
 }
